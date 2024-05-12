8a56eb372d3b5d2971567f943aa8d793febeac3d
JonM0 <61517667+JonM0@users.noreply.github.com>
2024-05-12 19:43:36 +0200

    Remove migrations file check

--- build.rs
+++ build.rs
@@ -22,8 +22,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &["proto", "proto/googleapis"],
         )?;
 
-    // rerun if migrations change
-    println!("cargo:rerun-if-changed=migrations");
-
     Ok(())
 }
